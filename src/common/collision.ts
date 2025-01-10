fbfcf8e73ce48175088ac61a62774ce29a6726a7
klikki-lab <klikki.lab@gmail.com>
2025-01-10 13:54:59 +0900

    collision.tsの修正

--- src/common/collision.ts
+++ src/common/collision.ts
@@ -6,17 +6,17 @@ export namespace Collision {
         return dx < (e1.width + e2.width) / 2 && dy < (e1.height + e2.height) / 2;
     };
 
-    export const within = (e1: g.E, e2: g.E) => {
+    export const withinArea = (e1: g.E, e2: g.E, widthRate = 1, heightRate = 1) => {
         const dx = Math.abs(e1.x - e2.x);
         const dy = Math.abs(e1.y - e2.y);
-        const w1 = Math.abs(e1.width * e1.scaleX);
-        const w2 = Math.abs(e2.width * e2.scaleX);
-        const h1 = Math.abs(e1.height * e1.scaleY);
-        const h2 = Math.abs(e2.height * e2.scaleY);
+        const w1 = Math.abs(e1.width * e1.scaleX * widthRate);
+        const w2 = Math.abs(e2.width * e2.scaleX * widthRate);
+        const h1 = Math.abs(e1.height * e1.scaleY * heightRate);
+        const h2 = Math.abs(e2.height * e2.scaleY * heightRate);
         return dx < (w1 + w2) / 2 && dy < (h1 + h2) / 2;
     };
 
-    export const withinArea = (e1: g.E, r1: number, e2: g.E, r2: number) => {
+    export const within = (e1: g.E, r1: number, e2: g.E, r2: number) => {
         const dx = e1.x - e2.x;
         const dy = e1.y - e2.y;
         const r = r1 + r2;
