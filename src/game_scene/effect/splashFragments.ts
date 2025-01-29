e68398e9abe2db7c73446e9e90d09fd17db6ab62
klikki-lab <klikki.lab@gmail.com>
2025-01-29 23:53:20 +0900

    軽微な修正

--- src/game_scene/effect/splashFragments.ts
+++ src/game_scene/effect/splashFragments.ts
@@ -2,10 +2,10 @@ export class SplashFragments {
 
     private static readonly MAX_NUM = 3;
 
-    constructor(scene: g.Scene, parent: g.Scene | g.E, entity: g.Sprite) {
+    constructor(scene: g.Scene, parent: g.Scene | g.E, entity: g.Sprite, scale: number = .3) {
         const offsetAngle = Math.PI / (g.game.random.generate() * SplashFragments.MAX_NUM);
         for (let i = 0; i < SplashFragments.MAX_NUM; i++) {
-            const fragment = new Fragments(scene, entity);
+            const fragment = new Fragments(scene, entity, scale);
             const rad = 2 * Math.PI * (i / SplashFragments.MAX_NUM) - offsetAngle;
             const v = fragment.width * fragment.scaleX * .5;
             const r = v + v * g.game.random.generate() * .5;
@@ -21,7 +21,7 @@ class Fragments extends g.Sprite {
 
     velocity: g.CommonOffset = { x: 0, y: 0 };
 
-    constructor(scene: g.Scene, entity: g.Sprite) {
+    constructor(scene: g.Scene, entity: g.Sprite, scale: number) {
         super({
             scene: scene,
             src: entity.src,
@@ -29,8 +29,8 @@ class Fragments extends g.Sprite {
             y: entity.y,
             anchorX: .5,
             anchorY: .5,
-            scaleX: .3,
-            scaleY: .3,
+            scaleX: scale,
+            scaleY: scale,
         });
 
         this.onUpdate.add(() => {
