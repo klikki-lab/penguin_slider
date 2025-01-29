1238f49f5e040122e8334588a69d713ada1fdf5b
klikki-lab <klikki.lab@gmail.com>
2025-01-29 23:55:59 +0900

    サプライズボーナス時のアニメーションを追加

--- src/game_scene/stage/snowFlake.ts
+++ src/game_scene/stage/snowFlake.ts
@@ -2,13 +2,14 @@ export class Snowflake extends g.Sprite {
 
     private _isObtained = false;
 
-    constructor(scene: g.Scene, assetId: string, private _score: number) {
+    constructor(scene: g.Scene, assetId: string, private _score: number, private isSurprise = false) {
         super({
             scene: scene,
             src: scene.asset.getImageById(assetId),
             anchorX: .5,
             anchorY: .5,
         });
+        this.scale(isSurprise ? 0.01 : 1);
 
         this.onUpdate.add(this.updateHandler);
     }
@@ -20,6 +21,13 @@ export class Snowflake extends g.Sprite {
     };
 
     private updateHandler = (): void | boolean => {
+        if (this.isSurprise) {
+            this.scale(this.scaleX *= 1.5);
+            if (this.scaleX > 1) {
+                this.scale(1);
+            }
+            this.modified();
+        }
         if (this._isObtained || this.x + this.width / 2 < (g.game.focusingCamera as g.Camera2D).x) {
             this.destroy();
             return true;
