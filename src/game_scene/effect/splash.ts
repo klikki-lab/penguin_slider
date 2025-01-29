1083d972644f7da04c4eedd88c23e9fe586728da
klikki-lab <klikki.lab@gmail.com>
2025-01-29 23:52:55 +0900

    コンストラクタの引数にbaseYを追加

--- src/game_scene/effect/splash.ts
+++ src/game_scene/effect/splash.ts
@@ -2,7 +2,7 @@ import { Entity } from "../entity/entity";
 
 export class Splash {
 
-    constructor(scene: g.Scene, parent: g.Scene | g.E, x: number) {
+    constructor(scene: g.Scene, parent: g.Scene | g.E, x: number, baseY: number) {
         const w = Entity.SIZE / 2;
         for (let i = 0; i < 3; i++) {
             const splash = new g.Sprite({
@@ -10,7 +10,7 @@ export class Splash {
                 parent: parent,
                 src: scene.asset.getImageById("img_splash"),
                 x: (x - w) + i * w,
-                y: g.game.height,
+                y: baseY,
                 anchorX: .5,
             });
 
@@ -20,7 +20,7 @@ export class Splash {
 
             let step = 0;
             splash.onUpdate.add(() => {
-                splash.y = g.game.height - Math.sin((++step / maxStep) * Math.PI) * top;
+                splash.y = baseY - Math.sin((++step / maxStep) * Math.PI) * top;
                 splash.modified();
                 if (step >= maxStep || splash.x + splash.width / 2 < (g.game.focusingCamera as g.Camera2D).x) {
                     splash.destroy();
